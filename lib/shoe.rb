829fe0dabd599d1bcfd378b40c9fdcf270634a91
Ashley Norton <nortonashleym@gmail.com>
2019-09-20 21:21:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    
+    BRANDS << @brand.uniq
   end
-  
-  def brand=(brand) 
-    @brand = brand 
-    BRANDS << brand.uniq
-  end 
 
   def cobble
     self.condition = "new"
